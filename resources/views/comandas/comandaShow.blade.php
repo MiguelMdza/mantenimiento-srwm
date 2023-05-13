0efdc1d59d79611708cc22b5566ea2751a77c74d
P4CM4N890 <p4cm4n890@gmail.com>
2023-05-13 13:44:15 -0600

    Traducción del Footer en todos los archivos

--- resources/views/comandas/comandaShow.blade.php
+++ resources/views/comandas/comandaShow.blade.php
@@ -135,40 +135,40 @@
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="footer-contact">
-                                    <h2>Our Address</h2>
-                                    <p><i class="fa fa-map-marker-alt"></i>123 Street, New York, USA</p>
-                                    <p><i class="fa fa-phone-alt"></i>+012 345 67890</p>
-                                    <p><i class="fa fa-envelope"></i>info@example.com</p>
+                                    <h2>Nuestra Información</h2>
+                                    <p><i class="fa fa-map-marker-alt"></i>Enrique Segoviano #13, JAL, MEX</p>
+                                    <p><i class="fa fa-phone-alt"></i>+52 3335681257</p>
+                                    <p><i class="fa fa-envelope"></i>olympus@gmail.com</p>
                                     <div class="footer-social">
-                                        <a href=""><i class="fab fa-twitter"></i></a>
-                                        <a href=""><i class="fab fa-facebook-f"></i></a>
-                                        <a href=""><i class="fab fa-youtube"></i></a>
-                                        <a href=""><i class="fab fa-instagram"></i></a>
-                                        <a href=""><i class="fab fa-linkedin-in"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-youtube"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="footer-link">
-                                    <h2>Quick Links</h2>
-                                    <a href="">Terms of use</a>
-                                    <a href="">Privacy policy</a>
-                                    <a href="">Cookies</a>
-                                    <a href="">Help</a>
-                                    <a href="">FQAs</a>
+                                    <h2>Enlaces</h2>
+                                    <a href="#">Términos y Condiciones</a>
+                                    <a href="#">Políticas de Privacidad</a>
+                                    <a href="#">Cookies</a>
+                                    <a href="#">Ayuda</a>
+                                    <a href="#">FQAs</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-5">
                         <div class="footer-newsletter">
-                            <h2>Newsletter</h2>
+                            <h2>Boletín Informátivo</h2>
                             <p>
-                                Lorem ipsum dolor sit amet elit. Quisque eu lectus a leo dictum nec non quam. Tortor eu placerat rhoncus, lorem quam iaculis felis, sed lacus neque id eros.
+                                Si quieres recibir novedades acerca de nuestro restaurante, envíamos tu correo.
                             </p>
                             <div class="form">
-                                <input class="form-control" placeholder="Email goes here">
-                                <button class="btn custom-btn">Submit</button>
+                                <input class="form-control" placeholder="El correo va aquí">
+                                <button class="btn custom-btn">Enviar</button>
                             </div>
                         </div>
                     </div>
@@ -176,8 +176,8 @@
             </div>
             <div class="copyright">
                 <div class="container">
-                    <p>Copyright &copy; <a href="#">Your Site Name</a>, All Right Reserved.</p>
-                    <p>Designed By <a href="https://htmlcodex.com">HTML Codex</a></p>
+                    <p>Derechos de Autor &copy; <a href="#">SRWM</a>, Todos los Derechos Revervados.</p>
+                    <p>Diseñado por <a href="#">Olympus </a></p>
                 </div>
             </div>
         </div>
